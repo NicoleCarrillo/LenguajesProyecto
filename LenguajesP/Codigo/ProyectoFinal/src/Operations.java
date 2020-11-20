f3c8acbbd5d271ae9018e5df61ef7f18825d2294
Nicole Carrillo C <A01551581@itesm.mx>
2020-11-19 21:29:49 -0500

    clase producer y operations inicializada

--- LenguajesP/Codigo/ProyectoFinal/src/Operations.java
+++ LenguajesP/Codigo/ProyectoFinal/src/Operations.java
@@ -1,4 +1,23 @@
 
 public class Operations {
+	
+	public Operations(int min,int max) {
+		
+	}
+
+	public int[] getRandomNumbers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getRandomOperation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getFormatOperation(int[] arreglo, String operacionRandom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- LenguajesP/Codigo/ProyectoFinal/src/Producer.java
+++ LenguajesP/Codigo/ProyectoFinal/src/Producer.java
@@ -1,36 +1,63 @@
-
-
+//PROYECTO 
 
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Producer extends Thread {
-    Buffer buffer;
     
-    Producer(Buffer buffer) {
+	Buffer buffer;
+	int rangoMenor;
+	int rangoMayor;
+	boolean ejecucion;
+    
+    Producer(Buffer buffer, int rMa, int rMe) {
         this.buffer = buffer;
+        this.rangoMayor = rMa;
+        this.rangoMenor = rMe;
+        this.ejecucion = true;
     }
     
     @Override
     public void run() {
         System.out.println("Running Producer...");
-        String products = "AEIOU";
-        Random r = new Random(System.currentTimeMillis());
-        char product;
         
-        for(int i=0 ; i<5 ; i++) {
-            product = products.charAt(r.nextInt(5));
-            this.buffer.produce(product);
-            //System.out.println("Producer produced: " + product);
-            Buffer.print("Producer produced: " + product);
-            
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        //METODO PROFE 
+        
+        //String products = "AEIOU";
+        //Random r = new Random(System.currentTimeMillis());
+        //char product;
+        
+//        for(int i=0 ; i<5 ; i++) {
+//            product = products.charAt(r.nextInt(5));
+//            this.buffer.produce(product);
+//            //System.out.println("Producer produced: " + product);
+//            Buffer.print("Producer produced: " + product);
+//            
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException ex) {
+//                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//        }
+        
+        while(this.ejecucion) {
+        	
+        	Operations objetoScheme = new Operations(rangoMayor,rangoMenor);
+        	int arreglo[]=objetoScheme.getRandomNumbers();
+        	String operacionRandom = objetoScheme.getRandomOperation();
+        	String formatOperation = objetoScheme.getFormatOperation(arreglo,operacionRandom);
+        	
+        	//PASAR formatOperation AL BUFFER 
+        	
+        	//TRY CATCH DEL BUFFER 
+        	
         }
+        
+    }
+    
+    public void stopThread() {
+    	this.ejecucion = false;
     }
     
 }
